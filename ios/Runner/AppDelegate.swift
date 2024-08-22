85a7eee7ca5e265c3c7a085432d23845c7e11260
Mohamed Ramzy <71365254+Ramzeus-svg@users.noreply.github.com>
2024-08-22 03:17:31 +0300

    NEW VERSION OF ETQAN

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,38 +1,13 @@
 import Flutter
 import UIKit
-import Firebase
-import UserNotifications
 
 @UIApplicationMain
-@objc class AppDelegate: FlutterAppDelegate, UNUserNotificationCenterDelegate {
+@objc class AppDelegate: FlutterAppDelegate {
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    print("App is launching")
-
-    FirebaseApp.configure()  // Initialize Firebase
-    print("Firebase configured")
-
-    // Request permission for notifications
-    UNUserNotificationCenter.current().delegate = self
-    let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
-    UNUserNotificationCenter.current().requestAuthorization(
-      options: authOptions,
-      completionHandler: { granted, error in
-        if let error = error {
-          print("Error requesting notification authorization: \(error)")
-        } else {
-          print("Notification authorization granted: \(granted)")
-        }
-      })
-
-    application.registerForRemoteNotifications()
-    print("Registered for remote notifications")
-
     GeneratedPluginRegistrant.register(with: self)
-    print("Plugins registered")
-
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
